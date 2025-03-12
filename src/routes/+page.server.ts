d949f3daf4739c9ea5e1b7005235e831edc0ee38
Boris <boris.krj@protonmail.com>
2025-03-12 19:41:00 +0100

    prijatli

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,1 +1,16 @@
-export async function load() {}
+import db from '$lib/db';
+
+export const actions = {
+	addFriend: async (event) => {
+		console.log(await event.request.formData());
+	}
+};
+
+export async function load() {
+	// SELECT * FROM friend
+	const friends = await db.friend.findMany();
+
+	return {
+		friends: friends
+	};
+}
